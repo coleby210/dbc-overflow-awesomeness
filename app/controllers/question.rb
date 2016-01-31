600f3fb1d336eab31a025fa59ba0483a19bdd09b
Amaar Fazlani <email": null>
2016-01-31 14:08:07 -0800

    Fix merge conflicts again

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -4,33 +4,33 @@ get "/questions/:id" do
   erb :"/questions/show"
 end
 
-get '/questions/new' do
-  @question = Question.new
+# get '/questions/new' do
+#   @question = Question.new
 
-  if request.xhr?
-    erb :"/questions/_new_question_partial", layout: false
-  else
-    erb :"/questions/_new_question_partial"
-  end
-end
+#   if request.xhr?
+#     erb :"/questions/_new_question_partial", layout: false
+#   else
+#     erb :"/questions/_new_question_partial"
+#   end
+# end
 
-post '/questions' do
-  @question = Question.new(params[:question])
+# post '/questions' do
+#   @question = Question.new(params[:question])
 
-  if request.xhr?
-    if @question.save
-      p "^" * 80
-      p @question
-      p "^" * 80
+#   if request.xhr?
+#     if @question.save
+#       p "^" * 80
+#       p @question
+#       p "^" * 80
 
-      erb :"/questions/_each_question_partial", layout: false, locals: {question: @question}
-    else
-      erb :"/questions/_show"
-    end
-  else
-    erb :"/questions/_each_question_partial"
-  end
-end
+#       erb :"/questions/_each_question_partial", layout: false, locals: {question: @question}
+#     else
+#       erb :"/questions/_show"
+#     end
+#   else
+#     erb :"/questions/_each_question_partial"
+#   end
+# end
 
 get '/questions/new' do
   if !logged_in?

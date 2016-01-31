c4e52d226fceee6d50d2d4d68caff33598adae83
Ovi Calvo & Michael <pair+0viWan.whelpley@devbootcamp.com>
2016-01-31 13:48:24 -0800

    user can create Question

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -2,3 +2,24 @@ get "/questions" do
   @questions = Question.all
   erb :"/"
 end
+
+get '/questions/new' do
+  if !logged_in?
+    redirect '/sessions/new'
+  else
+    @id = session[:id]
+    erb :'question/new'
+  end
+
+end
+
+# get inputs from form at 'questions/new.erb'
+post '/questions' do
+  @question = Question.new(params[:question])
+  if @question.save
+    redirect "/questions/#{@question.id}"
+  else
+    erb :'question/new'
+  end
+
+end

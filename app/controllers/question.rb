914634188fc671c0f4816c4caea65740b8ba7a17
Bernice Anne W. Chua <email": null>
2016-01-31 13:53:43 -0800

    Implement view of for loop of questions on front page.

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -1,4 +1,27 @@
-get "/questions" do
-  @questions = Question.all
-  erb :"/"
+get '/questions/new' do
+  @question = Question.new
+
+  if request.xhr?
+    erb :"/questions/_new_question_partial", layout: false
+  else
+    erb :"/questions/_new_question_partial"
+  end
+end
+
+post '/questions' do
+  @question = Question.new(params[:question])
+
+  if request.xhr?
+    if @question.save
+      p "^" * 80
+      p @question
+      p "^" * 80
+
+      erb :"/questions/_each_question_partial", layout: false, locals: {question: @question}
+    else
+      erb :"/questions/_show"
+    end
+  else
+    erb :"/questions/_each_question_partial"
+  end
 end

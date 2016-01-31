74ccdad3263101e3d9416a97c6d455fe32ac27d4
Bernice Anne W. Chua <email": null>
2016-01-31 13:58:12 -0800

    Merge work done by Michael W. & Ovi.

--- app/controllers/question.rb
+++ app/controllers/question.rb
@@ -25,3 +25,24 @@ post '/questions' do
     erb :"/questions/_each_question_partial"
   end
 end
+
+get '/questions/new' do
+  if !logged_in?
+    redirect '/sessions/new'
+  else
+    @id = session[:id]
+    erb :'question/new'
+  end
+
+end
+
+# get inputs from form at 'questions/new.erb'
+post '/questions' do
+  @question = Question.new(params[:question])
+  if @question.save
+    redirect "/questions/#{@question.id}"
+  else
+    erb :'question/new'
+  end
+
+end
